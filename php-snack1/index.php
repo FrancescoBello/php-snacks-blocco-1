f3de2a03277d8439c822c9171b2911bc527b07b6
Francesco <fraancesco.bello@gmail.com>
2020-12-15 17:10:52 +0100

    stampa partite

---
+++ php-snack1/index.php
@@ -0,0 +1,42 @@
+<?php
+$matches= [
+   [
+    "home"=>"Heat",
+    "visitors"=>"Pelicans",
+    "homePoint"=> "92",
+    "visitorsPoint"=>"114",
+  ],
+   [
+    "home"=>"Bulls",
+    "visitors"=>"Rockets",
+    "homePoint"=> "104",
+    "visitorsPoint"=>"125",
+  ],
+   [
+    "home"=>"Lakers",
+    "visitors"=>"Clippers",
+    "homePoint"=> "87",
+    "visitorsPoint"=>"81",
+  ],
+   [
+    "home"=>"Spurs",
+    "visitors"=>"Thunder",
+    "homePoint"=> "108",
+    "visitorsPoint"=>"121",
+  ],
+   [
+    "home"=>"Pistons",
+    "visitors"=>"Magic",
+    "homePoint"=> "112",
+    "visitorsPoint"=>"116",
+  ],
+
+
+];
+
+  for ($i=0; $i < count($matches) ; $i++) {
+    echo ($matches[$i]["home"] . "-" . $matches[$i]["visitors"] . " | " . $matches[$i]["homePoint"] . " - " . $matches[$i]["visitorsPoint"]);
+  }
+
+
+   ?>
